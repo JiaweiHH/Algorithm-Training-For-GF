1efadb2924b968a794511e8267e1179c23c868e1
phl <1871841289@qq.com>
2021-12-08 13:00:05 +0800

    12.8休息了2天

--- BTree/226-翻转二叉树.py
+++ BTree/226-翻转二叉树.py
@@ -18,3 +18,27 @@ class Solution(object):
         self.invertTree(root.left)
         self.invertTree(root.right)
         return root
+
+
+
+#不存在中间L的递归写法，直接交换，更加快，时间空间复杂度O(n)
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def invertTree(self, root):
+        """
+        :type root: TreeNode
+        :rtype: TreeNode
+        """
+        if root==None:
+            return root
+        left=self.invertTree(root.left)
+        right=self.invertTree(root.right)
+        root.right,root.left=left,right
+        return root
+
+#没有想出迭代的方法
