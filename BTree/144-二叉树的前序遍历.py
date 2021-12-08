809db837efdfb0274be34bc062ed1b85636a7fbc
phl <1871841289@qq.com>
2021-12-08 13:36:25 +0800

    耶耶

---
+++ BTree/144-二叉树的前序遍历.py
@@ -0,0 +1,51 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def preorderTraversal(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[int]
+        """
+        #根左右
+        res=[]
+        self.preorder(root,res)
+        return res
+    def preorder(self,root,res):
+        if root==None:
+            return
+        res.append(root.val)
+        self.preorder(root.left,res)
+        self.preorder(root.right,res)
+
+
+#以上是递归形式
+#以下是迭代方式
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def preorderTraversal(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[int]
+        """
+        stack=[]
+        res=[]
+        if root==None:
+            return res
+        while stack or root:
+            while root:
+                res.append(root.val)
+                stack.append(root)
+                root=root.left
+            root=stack.pop()
+            root=root.right
+        return res
